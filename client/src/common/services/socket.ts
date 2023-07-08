7e4676fbc20384977fea2a8aac182313672b1640
Jitender <jitender.chand1@tothenew.com>
2023-07-08 17:36:08 +0400

    feat: socket service design

---
+++ client/src/common/services/socket.ts
@@ -0,0 +1,40 @@
+import { EventEmitter } from "eventemitter3";
+
+export enum SocketServiceEvents {
+  CONNECTED = "connected",
+  DISCONNECTED = "disconnected",
+  ERROR = "error",
+}
+
+export class SocketService extends EventEmitter {
+  private static _instance: SocketService;
+  private _connection: WebSocket;
+  private _isConnected = false;
+
+  public static getInstance(): SocketService {
+    if (!SocketService._instance) {
+      SocketService._instance = new SocketService();
+    }
+    return SocketService._instance;
+  }
+  private constructor() {
+    super();
+    this._connection = new WebSocket("ws//localhost:5000");
+    this._connection.addEventListener("open", this._handleSocketConnection);
+    this._connection.addEventListener("close", this._handleSocketDisconnection);
+  }
+
+  private _handleSocketConnection() {
+    this._isConnected = true;
+    this.emit(SocketServiceEvents.CONNECTED, true);
+  }
+
+  private _handleSocketDisconnection() {
+    this._isConnected = false;
+    this.emit(SocketServiceEvents.DISCONNECTED, true);
+  }
+
+  get isConnected(): boolean {
+    return this.isConnected;
+  }
+}
